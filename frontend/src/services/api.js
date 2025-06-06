ef7c9464bb740c36b46e3d5d142acc490cc6aa06
Vikash Shakya <vikashshakya367@gmail.com>
2025-06-06 22:09:47 +0530

    commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true, // send cookies with requests
+  baseURL: window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : 'https://excel-analytics-backend-sigma.onrender.com/api',
+  withCredentials: true,
 });
-
 export default api;

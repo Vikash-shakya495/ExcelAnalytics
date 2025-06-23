6d3c4fe0277113d8b923403e15ef1c402318e054
Vikash Shakya <vikashshakya367@gmail.com>
2025-06-23 19:23:50 +0530

    commit

--- frontend/src/services/geminiService.js
+++ frontend/src/services/geminiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
+// const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDhwzrkKHG2RzdD1A6F0Ed9j1eibG9gxIk`;
 
 const generateInsights = async (data) => {
   const columns = Object.keys(data[0]);
